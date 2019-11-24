316ac73cbec27b8ab21ccc2f2fff9a02f44ccd29
Alex Krantz <alex@alexkrantz.com>
2019-11-24 07:59:53 -0800

    Add proton

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,10 @@ module github.com/kaifronsdal/FrontierHacks/backend
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/akrantz01/apcsp v0.0.0-20191008235722-641151a37d12
+	github.com/gorilla/mux v1.7.3
+	github.com/jinzhu/gorm v1.9.11
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/viper v1.5.0
+)
---
+++ backend/static/proton/precache-manifest.79f859bf0c4066b949699a3f3aea0fae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d47e379957b72d0bc59d2dbe39e80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3676067aaa75ba9308d",
+    "url": "/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "0f087f073a30e0ec91ec",
+    "url": "/static/css/main.4ef89df1.chunk.css"
+  },
+  {
+    "revision": "c3676067aaa75ba9308d",
+    "url": "/static/js/2.afcf4bec.chunk.js"
+  },
+  {
+    "revision": "0f087f073a30e0ec91ec",
+    "url": "/static/js/main.4400d0b2.chunk.js"
+  },
+  {
+    "revision": "c792a699fb883e50548f",
+    "url": "/static/js/runtime-main.46759097.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
